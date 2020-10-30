9f9e94b6658d4a3d5f384bf34bd6465102434e75
Ndagi Stanley <ndagis@gmail.com>
2020-10-30 14:38:01 +0300

    Add Changelog

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'A conversion package'
 LONG_DESCRIPTION = 'A package that makes it easy to convert values between several units of measurement'
 
